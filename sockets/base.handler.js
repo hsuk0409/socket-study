31b168f9cfda08f923816e1e822bd9ee3a40202a
chionsuk <hsuk0409@gmail.com>
2023-12-14 20:23:50 +0900

    emit 이름 변경

--- sockets/base.handler.js
+++ sockets/base.handler.js
@@ -2,7 +2,7 @@ module.exports = function (io, socket) {
   socket.on("socket:init", async function (data) {
     console.log(`>>>> [socket:init] data: ${JSON.stringify(data)} <<<<`);
 
-    await io.emit("connect", { code: "SUCCESS", data: 1 });
+    await io.emit("socket:test", { code: "SUCCESS", data: 1 });
   });
 
   socket.on("disconnect", async function (data) {
