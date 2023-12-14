92c9d1e54fe81f45626bb15c6d06f861b7c6789c
chionsuk <hsuk0409@gmail.com>
2023-12-14 20:18:10 +0900

    콜백함수 제거, init 시 테스트 emit 추가

--- sockets/base.handler.js
+++ sockets/base.handler.js
@@ -1,11 +1,11 @@
 module.exports = function (io, socket) {
-  socket.on("socket:init", async function (data, callBack) {
+  socket.on("socket:init", async function (data) {
     console.log(`>>>> [socket:init] data: ${JSON.stringify(data)} <<<<`);
 
-    return await callBack({ code: "SUCCESS", data: 1 });
+    await io.emit("connect", { code: "SUCCESS", data: 1 });
   });
 
-  socket.on("disconnect", async function (data, callBack) {
-    console.log(`>>>> [disconnect] data: ${JSON.stringify(data)} <<<<`);
+  socket.on("disconnect", async function (data) {
+    console.log(`>>>> [disconnect] data: ${JSON.stringify(data) || ""} <<<<`);
   });
 };
